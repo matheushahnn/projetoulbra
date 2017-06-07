cae380858b4c05d48221e729faceb3cf19a477ca
Matheus Hahn <hahnnmatheus@gmail.com>
2017-06-06 23:32:38 -0300

    teste agendaProfissional.

--- public/index.php
+++ public/index.php
@@ -58,16 +58,16 @@ $response->send();
 $kernel->terminate($request, $response);
 
 
-// $dbopts = parse_url(getenv('DATABASE_URL'));
-// $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-//                array(
-//                 'pdo.server' => array(
-//                    'driver'   => 'pgsql',
-//                    'user' => $dbopts["user"],
-//                    'password' => $dbopts["pass"],
-//                    'host' => $dbopts["host"],
-//                    'port' => $dbopts["port"],
-//                    'dbname' => ltrim($dbopts["path"],'/')
-//                    )
-//                )
-// );
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver'   => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
