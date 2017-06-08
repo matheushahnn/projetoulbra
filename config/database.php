2087e0ed16fabe14421cce80cb3790060401c50d
Matheus Hahn <hahnnmatheus@gmail.com>
2017-06-07 23:19:08 -0300

    debug

--- config/database.php
+++ config/database.php
@@ -75,16 +75,16 @@ return [
             'sslmode' => 'prefer',
         ],
 
-        // 'pgsql' => array(
-        //     'driver'   => 'pgsql',
-        //     'host'     => $host,
-        //     'database' => $database,
-        //     'username' => $username,
-        //     'password' => $password,
-        //     'charset'  => 'utf8',
-        //     'prefix'   => '',
-        //     'schema'   => 'public',
-        // ),
+        'pgsql' => array(
+            'driver'   => 'pgsql',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ),
 
     ],
 
